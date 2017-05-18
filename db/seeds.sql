89a14a97cbce8145b3ff3378497d17074d507c6a
Liz Rodriguez <ecrodriguez@gmail.com>
2017-05-18 14:25:53 -0400

    db stuff

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,7 @@
 INSERT INTO photos (filename, mimetype, path) VALUES
-('IMG_0251.JPG', 'image/jpeg', 'uploads/IMG_0251.JPG'),
-('IMG_0251.mov', 'video/quicktime', 'uploads/IMG_0251.mov'),
 ('IMG_0979.JPG', 'image/jpeg', 'uploads/IMG_0979.JPG'),
-('IMG_0979.mov', 'video/quicktime', 'uploads/IMG_0979.mov'),
+('IMG_0251.JPG', 'image/jpeg', 'uploads/IMG_0251.JPG'),
+('IMG_0811.JPG', 'image/jpeg', 'uploads/IMG_0811.JPG'),
 ('IMG_0983.JPG', 'image/jpeg', 'uploads/IMG_0983.JPG'),
-('IMG_0983.mov', 'video/quicktime', 'uploads/IMG_0983.mov'),
-('IMG_0991.JPG', 'image/jpeg', 'uploads/IMG_0991.JPG'),
-('IMG_0991.mov', 'video/quicktime', 'uploads/IMG_0991.mov');
+('IMG_0251.JPG', 'image/jpeg', 'uploads/IMG_0251.JPG'),
+('IMG_0995.JPG', 'image/jpeg', 'uploads/IMG_0995.JPG');
